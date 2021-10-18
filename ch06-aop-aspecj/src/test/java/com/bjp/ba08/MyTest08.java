22212823eff8c22c074219ca1b4290a86be5cf2f
Williamxh <821602702@qq.com>
2021-10-18 16:00:03 +0800

    有接口的也能使用cglib动态代理，在xml配置文件中加入proxy-target-class="true"即可

---
+++ ch06-aop-aspecj/src/test/java/com/bjp/ba08/MyTest08.java
@@ -0,0 +1,28 @@
+package com.bjp.ba08;
+
+import com.bjp.ba08.SomeService;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Willam_xh
+ * @create 2021-10-14 18:55
+ */
+
+public class MyTest08 {
+    @Test
+    public void test02(){
+        String config="applicationContext.xml";
+        ApplicationContext context = new ClassPathXmlApplicationContext(config);
+        //从容器中获取目标对象
+        SomeService proxy= (SomeService) context.getBean("someService");
+        //通过代理的对象执行方法，实现目标方法执行时，才增强了功能
+
+        System.out.println("proxy:"+proxy.getClass().getName());
+
+        proxy.doThird();
+
+
+    }
+}
