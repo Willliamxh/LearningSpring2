07053eb8b1a0dfe2c8fa7f59fd55d7a94c0ea59b
Williamxh <821602702@qq.com>
2021-10-18 15:52:07 +0800

    当目标类没有接口的时候 用的就是cglib的动态代理

---
+++ ch06-aop-aspecj/src/main/java/com/bjp/ba07/SomeServiceImpl.java
@@ -0,0 +1,17 @@
+package com.bjp.ba07;
+
+import com.bjp.ba02.Student;
+
+/**
+ * @author Willam_xh
+ * @create 2021-10-14 17:15
+ * 这个是目标类，也就是被代理类
+ */
+public class SomeServiceImpl {
+
+    public void doThird() {
+        System.out.println("执行业务方法doThird");
+    }
+}
+
+
--- ch06-aop-aspecj/src/test/java/com/bjp/ba07/MyTest06.java
+++ ch06-aop-aspecj/src/test/java/com/bjp/ba07/MyTest06.java
@@ -1,6 +1,6 @@
-package com.bjp.ba06;
+package com.bjp.ba07;
 
-import com.bjp.ba06.SomeService;
+
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -13,12 +13,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MyTest06 {
     @Test
     public void test02(){
+
         String config="applicationContext.xml";
         ApplicationContext context = new ClassPathXmlApplicationContext(config);
         //从容器中获取目标对象
-        SomeService proxy= (SomeService) context.getBean("someService");
+        SomeServiceImpl proxy= (SomeServiceImpl) context.getBean("someService");
         //通过代理的对象执行方法，实现目标方法执行时，才增强了功能
 
+        /**
+         * 目标类没有借口的时候，使用的是cglib的动态代理，Spring框架会自动应用cglib
+         * proxy:com.bjp.ba07.SomeServiceImpl$$EnhancerBySpringCGLIB$$c8c1e7aa
+         */
         System.out.println("proxy:"+proxy.getClass().getName());
 
         proxy.doThird();
