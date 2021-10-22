ee825e2511e4248fb5da9eaf2bddecc4bb0660cd
Williamxh <821602702@qq.com>
2021-10-22 14:50:17 +0800

    注解事务2-over Transactional 是spring自带的事务机制，适合小型项目。

--- ch09-spring-trans-anno/src/main/java/com/bjp/service/impl/BuyGoodServiceImpl.java
+++ ch09-spring-trans-anno/src/main/java/com/bjp/service/impl/BuyGoodServiceImpl.java
@@ -21,19 +21,31 @@ public class BuyGoodServiceImpl implements BuyGoodService {
     private SaleDao saleDao;
     private GoodsDao goodsDao;
 
-    @Transactional(
-            //传播方式
-            propagation=Propagation.REQUIRED,
-            //隔离状态
-            isolation = Isolation.DEFAULT,
-            //用于设置该方法对数据库的操作是否是只读的
-            readOnly = false,
-            //指定需要回滚的异常类,发生指定的异常一定回滚
-            rollbackFor = {
-                    NullPointerException.class,
-                    NotEnoughException.class
-            }
-    )
+    /**
+     * rollbackFor:表示发生指定异常一定回滚
+     *  处理逻辑：
+     *  1.spring框架会首先检查方法抛出的异常是不是在roolbackFor的属性值中
+     *  如果异常在rollbackfor 的列表中，那不管是什么类型的异常，一定回滚。
+     *  2.如果你抛出的异常不在roolbackfor列表中，spring会判断异常是不是RuntimeException，
+     *  是就回滚
+     */
+    // @Transactional(//这个spring自带的事务的机制就aop 环绕通知
+    //         //传播方式
+    //         propagation=Propagation.REQUIRED,
+    //         //隔离状态
+    //         isolation = Isolation.DEFAULT,
+    //         //用于设置该方法对数据库的操作是否是只读的
+    //         readOnly = false,
+    //         //指定需要回滚的异常类,发生指定的异常一定回滚
+    //         rollbackFor = {
+    //                 NullPointerException.class,
+    //                 NotEnoughException.class
+    //         }
+    // )
+    // 其实加一个默认的注解就能完成事务的控制 就是使用的是事务控制的默认值
+    // 默认的传播行为是REQUIRED 默认的隔离级别DEFAULT 默认抛出运行时异常，回滚事务
+    // 事务都是加在公共方法上的  事务的机制就是环绕通知
+    @Transactional
     @Override
     public void buy(Integer goodsId, Integer nums) {
         System.out.println("=====buy 方法的开始======");
